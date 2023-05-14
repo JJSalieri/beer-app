03905102f53248c10c81267af544c3d74cedce48
JJSalieri <salieri49.2006@gmail.com>
2023-05-14 21:22:13 +0400

    mini-patch

--- src/components/PaginationHandler.js
+++ src/components/PaginationHandler.js
@@ -27,7 +27,7 @@ export default function PaginationHandler() {
       </select>
       <CardList limit={Limit} page={Page}/>
       <div className="w-screen flex justify-center my-8 flex-wrap">{pagelist.map((page) =>{
-        return <span key={page} className={`mx-2 cursor-pointer ${page === Page? 'bg-slate-400':'bg-slate-300'} py-1 px-2`} onClick={() => setPage(page)}>{page}</span>
+        return <span key={page} className={`mx-2 my-1 cursor-pointer ${page === Page? 'bg-slate-400':'bg-slate-300'} py-1 px-2`} onClick={() => setPage(page)}>{page}</span>
       })}</div>
     </div>
   );
